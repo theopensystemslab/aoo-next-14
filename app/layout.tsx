89e7605b5fd3f520a354d9d897e97b91a41587a4
Thomas O'Neill <tom@opensystemslab.io>
2024-01-22 09:11:28 +0000

    EntryPopout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // import { useRouter } from "next/router"
 import "./globals.css"
+import EntryPopout from "./ui/EntryPopout"
 import MapboxGlobeServerSide from "./ui/map/MapboxGlobeServerSide"
 // import Footer from "../Footer"
 // import Header from "../Header"
@@ -10,24 +11,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <body>
       <div className="min-w-full fixed inset-0 overflow-y-auto overflow-x-hidden">
         {/* <Sidebar /> */}
-        {/* <motion.div
-      className="h-full w-full sm:w-1/2 absolute top-0 bottom-0 right-0 bg-white overflow-y-auto no-scrollbar z-50"
-      variants={{
-        open: {
-          x: "0%",
-        },
-        closed: {
-          x: "100%",
-        },
-      }}
-      // animate={entryOpen ? "open" : "closed"}
-      initial="closed"
-      transition={{
-        duration: 1,
-      }}
-    > */}
-        {children}
-        {/* </motion.div> */}
+        <EntryPopout>{children}</EntryPopout>
         <div className="min-w-full fixed inset-0 overflow-y-auto overflow-x-hidden">
           {/* <Header /> */}
           <div className="w-full h-screen max-h-[85vh] relative">
