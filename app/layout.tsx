306a3eb0bde1027a780e7343eee1600606b12c2b
Thomas O'Neill <tom@opensystemslab.io>
2024-01-24 09:06:15 +0000

    submit button; tailwind stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
-// import { useRouter } from "next/router"
 import "./globals.css"
 import BetaBanner from "./ui/BetaBanner"
 import EntryPopout from "./ui/EntryPopout"
 import Footer from "./ui/footer/Footer"
 import Header from "./ui/header/Header"
+import SubmitButton from "./ui/layout/SubmitButton"
 import MapboxGlobeServerSide from "./ui/map/MapboxGlobeServerSide"
 import Sidebar from "./ui/sidebar/Sidebar"
-// import Footer from "../Footer"
-// import Header from "../Header"
-// import Sidebar from "../sidebar/Sidebar"
+import { Inter } from "next/font/google"
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+})
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
+  <html lang="en" className={inter.className}>
     <body>
       <div className="min-w-full fixed inset-0 overflow-y-auto overflow-x-hidden">
         <Sidebar />
@@ -21,8 +24,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
           <Header />
           <div className="w-full h-screen max-h-[85vh] relative">
             <MapboxGlobeServerSide />
-            {/* <MapboxGlobe {...{ entries }} /> */}
-            {/* <SubmitButton /> */}
+            <SubmitButton />
           </div>
           <Footer />
         </div>
