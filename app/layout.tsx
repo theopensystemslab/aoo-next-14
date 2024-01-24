2dfad96d23d2ae783eacfb7618307543790918cb
Thomas O'Neill <tom@opensystemslab.io>
2024-01-24 07:53:31 +0000

    header, beta banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 // import { useRouter } from "next/router"
 import "./globals.css"
+import BetaBanner from "./ui/BetaBanner"
 import EntryPopout from "./ui/EntryPopout"
 import Footer from "./ui/footer/Footer"
+import Header from "./ui/header/Header"
 import MapboxGlobeServerSide from "./ui/map/MapboxGlobeServerSide"
 import Sidebar from "./ui/sidebar/Sidebar"
 // import Footer from "../Footer"
@@ -15,7 +17,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
         <Sidebar />
         <EntryPopout>{children}</EntryPopout>
         <div className="min-w-full fixed inset-0 overflow-y-auto overflow-x-hidden">
-          {/* <Header /> */}
+          <BetaBanner />
+          <Header />
           <div className="w-full h-screen max-h-[85vh] relative">
             <MapboxGlobeServerSide />
             {/* <MapboxGlobe {...{ entries }} /> */}
